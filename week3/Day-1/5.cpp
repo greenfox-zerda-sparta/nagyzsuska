4924d699bdc3766d9fc0fbf1b18b84c9609e28e0
Nagy Zsuzsanna <nagyzsuska@live.com>
2016-11-02 18:13:20 +0100

    Week3 day1 5.

---
+++ week3/Day-1/5.cpp
@@ -0,0 +1,55 @@
+//============================================================================
+// Name        : 5.cpp
+// Author      : Zsuzsanna Nagy
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Create a function called "resize" that takes a pointer to an int array, it's size and it's new size,
+ * then it should return a new pointer that points to the new resized array.
+ * It should copy all the elements to the new array, if the array is bigger it should fill all the new slots with zeros.
+ * It should delete the old array.
+ */
+
+int* resize(int* array, int size, int new_size) {
+  int* resized = nullptr;
+  resized = new int[new_size];
+  for (int i = 0; i < new_size; i++) {
+    if (i < size) {
+      resized[i] = array[i];
+    } else {
+      resized[i] = 0;
+    }
+  }
+  return resized;
+  delete []resized;
+  resized = nullptr;
+ }
+
+int main() {
+  int size = 10;
+  int new_size = 15;
+  int *array = nullptr;
+  array = new int[size];
+  for(int i = 0; i < size; i++){
+    array[i] = i;
+  }
+  cout << "Elements of the first array: " << endl;
+  for (int i = 0; i < size; i++) {
+       cout << array[i] << endl;
+   }
+  array = resize(array, size, new_size);
+  cout << "Elements of the resized array: " << endl;
+  for (int i = 0; i < new_size; i++) {
+       cout << array[i] << endl;
+     }
+  delete []array;
+  array = nullptr;
+  return 0;
+}
